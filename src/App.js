4b836b30f27244f3267b4895eafb9bdfe842b056
shivibhardwaj29 <shivi.bhardwaj@highwirepress.com>
2025-06-24 00:25:22 +0530

    Auth.js: Create signup & signin

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
-import Header from "./components/Header";
-import TaskList from "./pages/TaskList";
+import Auth from "./auth/Auth";
+// import SignUp from "./auth/SignUp";
+// import Header from "./components/Header";
+// import TaskList from "./pages/TaskList";
+
 
 const App = () => {
   return (
-    <div className="min-h-screen px-4 py-6 md:px-12 bg-gradient-to-b from-blue-100 to-white">
-      <Header />
-      <main className="max-w-3xl mx-auto">
-        <TaskList />
-      </main>
+    <div className="min-h-screen">
+      <Auth />
     </div>
   );
 };
